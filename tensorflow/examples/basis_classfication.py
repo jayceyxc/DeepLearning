fe8ab8f0adb4eb1aa111a25c28e552d8efc002a5
xinluomed_yuxuecheng <xinluomed_yuxuecheng@git.cloud.tencent.com>
2018-11-13 19:52:33 +0800

    Initial commit

---
+++ tensorflow/examples/basis_classfication.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# @Time    : 2018/11/13 19:35
+# @Author  : yuxuecheng
+# @Contact : yuxuecheng@xinluomed.com
+# @Site    : 
+# @File    : basis_classfication.py
+# @Software: PyCharm
+# @Description https://tensorflow.google.cn/tutorials/keras/basic_classification
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
