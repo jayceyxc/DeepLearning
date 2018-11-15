d9a38a68d089f556aaeca8ffbe3bbb43bd01e9af
xinluomed_yuxuecheng <xinluomed_yuxuecheng@git.cloud.tencent.com>
2018-11-15 21:47:55 +0800

    Finish the overfit and underfit

--- tensorflow_code/examples/overfit_and_underfit/overfit_and_underfit.py
+++ tensorflow_code/examples/overfit_and_underfit/overfit_and_underfit.py
@@ -116,3 +116,44 @@ def plot_history(histories, key='binary_crossentropy'):
 plot_history([('baseline', baseline_history),
               ('smaller', smaller_history),
               ('bigger', bigger_history)])
+
+# Add weight regularization
+l2_model = keras.models.Sequential([
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation=tf.nn.relu, input_shape=(NUM_WORDS, )),
+    keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
+                       activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+l2_model.compile(optimizer=keras.optimizers.Adam(),
+                 loss=keras.losses.binary_crossentropy,
+                 metrics=['accuracy', 'binary_crossentropy'])
+l2_model_history = l2_model.fit(train_data,
+                                train_labels,
+                                epochs=20,
+                                batch_size=512,
+                                validation_data=(test_data, test_labels),
+                                verbose=2)
+
+plot_history([('baseline', baseline_history),
+              ('l2', l2_model_history)])
+
+# Add dropout
+dropout_model = keras.models.Sequential([
+    keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(16, activation=tf.nn.relu),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+dropout_model.compile(optimizer=keras.optimizers.Adam(),
+                      loss=keras.losses.binary_crossentropy,
+                      metrics=['accuracy','binary_crossentropy'])
+dropout_model_history = dropout_model.fit(train_data,
+                                          train_labels,
+                                          epochs=20,
+                                          batch_size=512,
+                                          validation_data=(test_data, test_labels),
+                                          verbose=2)
+plot_history([('baseline', baseline_history),
+              ('dropout', dropout_model_history)])
